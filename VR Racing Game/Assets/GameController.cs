19f2f765a46aefe60029dc2a519ab562e18f9439
Ben Savage <Finnegan2100@protonmail.com>
2018-07-08 22:07:53 -0700

    Added ramps & checkpoints. Set up basic kart movement & collision detection.

---
+++ VR Racing Game/Assets/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public TextMesh infoText;
+
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		infoText.text = "Time: " + Mathf.Floor(timer);
+	}
+}
---
+++ VR Racing Game/Assets/KartScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KartScript : MonoBehaviour {
+
+	public float speed = 1f;
+	public bool levelBegin = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began || Input.GetKeyDown ("space")) {
+			levelBegin = true;
+		}
+
+		if (levelBegin) {
+
+			Vector3 movementDirection = new Vector3 (
+				                           transform.forward.x,
+				                           0,
+				                           transform.forward.z
+			                           );
+			transform.position += movementDirection.normalized * speed * Time.deltaTime;
+		}
+	}
+
+	void OnTriggerEnter(Collider collider) {
+
+		if (collider.tag == "Checkpoint") {
+			int checkpoint = int.Parse(collider.name);
+		}
+	}
+}
+
